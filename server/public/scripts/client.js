679886aa63d6e6310b16194750504a2adeef2180
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-16 17:39:07 -0500

    added click functionality to buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,3 +1,20 @@
 $(function(){
   console.log("Jquery Ready!");
+//Button functionality
+//buttons for adding items
+  $("#addItemButton").on('click', function(){
+    console.log("Add Item Button Clicked");
+  });
+  //buttons for searching items
+  $("#searchByNameButton").on('click', function(){
+    console.log("Search by name button clicked");
+  });
+  $("#searchByDescriptionButton").on('click', function(){
+    console.log("Search by Description Button clicked");
+  });
+
+  //clear buttons
+  $("#clearSearchButton").on('click', function(){
+    console.log("Clear search button clicked");
+  });
 });
